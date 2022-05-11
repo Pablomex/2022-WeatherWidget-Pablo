6b04baa8f00c3321845baa7f1d3087f02b453782
Pierre-Nicolas <pnpvdc@gmail.com>
2022-05-11 16:01:24 +0200

    Weather Widget API

---
+++ README.md
@@ -0,0 +1,8 @@
+# Widget météo en React
+
+Porjet de widget météo en React avec connexion à l'API Open Weather
+
+Utilisation de React, SCSS et Open Weather API
+
+## Le GIF qui va bien
+
---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+import WeatherWidget from '../WeatherWidget';
+
+function App() {
+  return (
+    <div className="app">
+      <WeatherWidget />
+    </div>
+  );
+}
+
+export default App;
